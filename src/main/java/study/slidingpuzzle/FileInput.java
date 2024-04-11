ba030e0783e8619041725d19caa2ecb4ba8be786
dilmi214 <dilmifernando214@gmail.com>
2024-04-11 15:25:12 +0530

    readFile method

--- src/main/java/study/slidingpuzzle/FileInput.java
+++ src/main/java/study/slidingpuzzle/FileInput.java
@@ -7,8 +7,22 @@ package study.slidingpuzzle;
 
 /**
  *
- * @author Acer
+ * @author Dilmi
  */
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileInput {
-    
+    public static void readFile(String fileName){
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+            String line;
+            while ((line=reader.readLine())!= null){
+                System.out.println(line);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
